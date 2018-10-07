0996345e788f64194d01daeec8eb77feb91fa7d9
Andrew Drummond <addrummond61@gmail.com>
2018-10-07 13:26:49 -0400

    initialized frontend provider

--- client/web3/provider.js
+++ client/web3/provider.js
@@ -1,22 +1,21 @@
-import contract from 'truffle-contract'
 import Web3 from "web3"
 
-const provider = () => {
-  // If the user has MetaMask:
-  if (typeof web3 !== 'undefined') {
-    return web3.currentProvider
-  } else {
-    console.error("You need to install MetaMask for this app to work!")
+let web3, coinbase = null
+
+const getProvider = async () => {
+  if (!window.web3) {
+    window.alert('Please install MetaMask first.')
+    return;
   }
-}
+  if (!web3) {
+    web3 = new Web3(window.web3.currentProvider)
+  }
+  coinbase = await web3.eth.getCoinbase((err, coinbase) => {
+    return coinbase || err
+  })
+};
 
-export const eth = new Web3(provider()).eth
+getProvider()
 
-export const getInstance = async artifact => {
-  const contractObj = contract(artifact)
-  contractObj.setProvider(provider());
-
-  const deployed = await contractObj.deployed();
-  console.log(deployed);
-  return deployed;
-}
+export default web3
+export coinbase
