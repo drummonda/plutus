aff237bdec7ceb7dba917be7cdddb0b98a2e2c80
Andrew Drummond <addrummond61@gmail.com>
2018-10-08 12:46:45 -0400

    removed comments

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,65 +1,42 @@
 const path = require("path");
 const fs = require("fs-extra");
 const solc = require("solc");
-
-// Store the build folder and the contract file
 const buildPath = path.resolve(__dirname, "..", "build");
 const contractsPath = path.resolve(__dirname, "..", "contracts");
 const artifactsPath = path.resolve(__dirname, "..", "client", "artifacts");
 let contractObjects = [];
 
 const compileContract = contract => {
-  // Compile the contract code, return the contract object
   const compiled = solc.compile(contract, 1);
   const compiledContract = compiled.contracts;
-
-  // Console log for the user
   console.log("Compilation output", compiled);
-
-  // Return the ABI for use
   return compiledContract
 }
 
 const writeContractArtifact = (compiledContract, ABI, contractName) => {
-  // Build the new contract fileName
   const fileName = `${contractName}.json`;
   const key = `:${contractName}`;
-  // Create a JSON file with the exhibition contract
   fs.outputJsonSync(
     path.resolve(buildPath, fileName),
     compiledContract[key]
   );
-
-  // Create a JSON file with the abi interface
   fs.outputJsonSync(
     path.resolve(artifactsPath, fileName),
     ABI
   );
-
-  // Add to the exported object
   contractObjects = [...contractObjects, compiledContract[key]];
 }
 
-// Remove the current build directory
 fs.removeSync(buildPath);
-
-// Ensures that the directory build exists
 fs.ensureDirSync(buildPath);
 fs.ensureDirSync(artifactsPath);
 
-// Read all contract files, compile them and write their artifacts
 fs.readdirSync(contractsPath).forEach(file => {
-  // Grab the fileName, for writing new file purposes
   const contractName = file.split('.').slice(0, 1)[0];
-  // Grab the current file's filepath
   const filePath = path.resolve(contractsPath, file);
-  // Read the contract contents
   const contract = fs.readFileSync(filePath, 'utf8');
-  // Compile the contract
   const compiledContract = compileContract(contract);
-  // Grab the compiledContract's interface
   const ABI = JSON.parse(compiledContract[`:${contractName}`].interface);
-  // Write the contract artifact to file
   writeContractArtifact(compiledContract, ABI, contractName);
 });
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,46 +1,31 @@
-// Grab the web3 module for interacting with smart contracts
 const Web3 = require("web3");
 const Provider = require("truffle-hdwallet-provider");
-
-// Grab array of compiled contracts
 const compiledContracts = require("./compile");
-console.log("compiled contracts", compiledContracts.length);
-
-// Create a new instance of web3 with the correct provider
 const web3 = new Web3("http://localhost:8545");
 
-// This function is used to deploy the contract
 const deploy = async (accounts, ABI, bytecode) => {
   try {
-    // Create a contract with the ABI, deploy with bytecode
     const contract = await new web3.eth.Contract(JSON.parse(ABI));
     const deployed = await contract.deploy({ data: bytecode });
     const sent = await deployed.send({
      from: accounts[0],
      gas: 3000000,
    });
-
     const address = sent.options.address;
     console.log("Deployed on:", address);
     return;
-
   } catch (err) {
     console.error(err);
   }
 }
 
+// This function loops over all compiled contracts and deploys them
 const deployAllContracts = async () => {
   try {
-    // Get a list of accounts
     const accounts = await web3.eth.getAccounts();
-
-    // Go through each contract in array and deploy
     compiledContracts.map(async contract => {
-      // Interface and bytcode object from compiled contract
       const { interface, bytecode } = contract;
       const ABI = interface;
-
-      // Deploy the contract
       await deploy(accounts, ABI, bytecode);
     })
   } catch (err) {
