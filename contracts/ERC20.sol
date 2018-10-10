5c9186b787e8686ab0d4972c19a0eaae02e8d802
Andrew Drummond <addrummond61@gmail.com>
2018-10-10 12:19:27 -0400

    completed erc20 functionality

---
+++ contracts/ERC20.sol
@@ -0,0 +1,188 @@
+pragma solidity ^0.4.25;
+
+import './Owned.sol';
+
+contract ERC20 is Owned {
+
+    /* -------------- Contract variables --------------*/
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    uint256 public sellPrice;
+    uint256 public buyPrice;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* -------------- Contract events --------------*/
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Burn(address indexed from, uint256 value);
+
+
+    /* -------------- Constructor function --------------*/
+    function constructor (
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    {
+        totalSupply = initialSupply * 10 * uint256(decimals);
+        balanceOf[msg.sender] = initialSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
+
+
+    /**
+     * Internal transfer, can oly be called by this contract
+     */
+    function _transfer (address _from, address _to, uint _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens `_to` from your account
+     *
+     * @param _to the address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer (address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens `_to` in behalf of `_from`
+     *
+     * @param _from the address of the sender
+     * @param _to the address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom (address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender the address authorized to spend
+     * @param _value the amount to send
+     */
+    function approve (address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+
+     /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender the address authorized to spend
+     * @param _value the amount to send
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall (address _spender, uint256 _value, bytes _extraData) public returns(bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApprovae(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+
+     /**
+     * Destroy tokens from the other account
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn (uint256 _value) public returns (bool success) {
+        require(balanceOf[_msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+
+     /**
+     * Destroy tokens from the other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom (address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(_from, _value);
+        return true;
+    }
+
+
+
+    /* -------------- Mint tokens --------------*/
+    function mintToken (address target, uint256 mintedAmount) onlyOwner {
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        emit Transfer(0, owner, mintedAmount);
+        emit Transfer(owner, target, mintedAmount);
+    }
+
+
+    /* -------------- Set prices --------------*/
+    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner {
+        sellPrice = newSellPrice;
+        buyPrice = newBuyPrice;
+    }
+
+
+    /* -------------- Contract buys tokens --------------*/
+    function buy() payable returns (uint amount) {
+        amount = msg.value / buyPrice;
+        _transfer(this, msg.sender, amount);
+        return amount;
+    }
+
+
+    /* -------------- Contract sells tokens --------------*/
+    function sell(uint amount) returns (uint revenue) {
+        require(balanceOf[msg.sender] >= amount);
+        balanceOf[this] += amount;
+        balanceOf[msg.sender] -= amount;
+        revenue = amount * sellPrice;
+        msg.sender.transfer(revenue);
+        Transfer(msg.sender, this, amount);
+        return revenue;
+    }
+
+}
--- contracts/PeerToken.sol
+++ contracts/PeerToken.sol
@@ -1,67 +1,7 @@
 pragma solidity ^0.4.25;
 
-import './Owned.sol';
+import './ERC20.sol';
 
-contract PeerToken is Owned {
-
-    /* -------------- Contract events --------------*/
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-
-    /* -------------- Contract variables --------------*/
-    mapping (address => uint256) public balanceOf;
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-
-
-    /* -------------- Constructor function --------------*/
-    function constructor(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol,
-        uint8 decimalUnits,
-        address centralMinter) public
-    {
-        if(centralMinter != 0) owner = centralMinter;
-        balanceOf[msg.sender] = initialSupply;
-        name = tokenName;
-        symbol = tokenSymbol;
-        decimals = decimalUnits;
-    }
-
-
-    /* -------------- Mint tokens --------------*/
-    function mintToken(address target, uint256 mintedAmount) onlyOwner {
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        emit Transfer(0, owner, mintedAmount);
-        emit Transfer(owner, target, mintedAmount);
-    }
-
-
-    /* -------------- Transfer tokens --------------*/
-    function transfer(address _to, uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
-    }
-
-
-    /* -------------- Internal transfer --------------*/
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
-        require(!frozenAccount[_from]);
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-    }
-
+contract PeerToken is ERC20 {
 
 }
