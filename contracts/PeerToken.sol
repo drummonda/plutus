5b66e987ebf076f908809bb9f610a5825a19bd5e
Andrew Drummond <addrummond61@gmail.com>
2018-10-10 17:55:06 -0400

    fixed compile error

--- contracts/PeerToken.sol
+++ contracts/PeerToken.sol
@@ -1,7 +1,171 @@
 pragma solidity ^0.4.25;
 
-import './ERC20.sol';
-import './Owned.sol';
+
+contract Owned {
+    address public owner;
+
+    function owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+interface tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external;
+}
+
+contract ERC20 {
+
+    /* -------------- Contract variables --------------*/
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* -------------- Contract events --------------*/
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Burn(address indexed from, uint256 value);
+
+
+    /* -------------- Constructor function --------------*/
+    constructor (
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol)
+    {
+        totalSupply = initialSupply * 10 * uint256(decimals);
+        balanceOf[msg.sender] = initialSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
+
+
+    /**
+     * Internal transfer, can oly be called by this contract
+     */
+    function _transfer (address _from, address _to, uint _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens `_to` from your account
+     *
+     * @param _to the address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer (address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens `_to` in behalf of `_from`
+     *
+     * @param _from the address of the sender
+     * @param _to the address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom (address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender the address authorized to spend
+     * @param _value the amount to send
+     */
+    function approve (address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+
+     /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender the address authorized to spend
+     * @param _value the amount to send
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall (address _spender, uint256 _value, bytes _extraData) public returns(bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+
+     /**
+     * Destroy tokens from the other account
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn (uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+
+     /**
+     * Destroy tokens from the other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom (address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(_from, _value);
+        return true;
+    }
+
+}
 
 contract PeerToken is Owned, ERC20 {
 
@@ -11,7 +175,7 @@ contract PeerToken is Owned, ERC20 {
 
 
     /* -------------- Constructor function --------------*/
-    function constructor (
+    constructor (
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
@@ -59,7 +223,7 @@ contract PeerToken is Owned, ERC20 {
      *
      * @notice Buy tokens from contract by sending ether
     */
-    function buy() payable returns (uint amount) {
+    function buy() public payable returns (uint amount) {
         amount = msg.value / buyPrice;
         _transfer(this, msg.sender, amount);
     }
@@ -70,11 +234,11 @@ contract PeerToken is Owned, ERC20 {
      * @notice Sell `amount` tokens to contract
      * @param amount amount of tokens to be sold
     */
-    function sell(uint amount) returns (uint revenue) {
+    function sell(uint amount) public returns (uint revenue) {
         address myAddress = this;
         require(myAddress.balance >= amount * sellPrice);
         _transfer(msg.sender, this, amount);
         msg.sender.transfer(revenue);
-        Transfer(msg.sender, this, amount);
+        emit Transfer(msg.sender, this, amount);
     }
 }
--- scripts/migrate.js
+++ scripts/migrate.js
@@ -3,24 +3,6 @@ const Provider = require("truffle-hdwallet-provider");
 const compiledContracts = require("./compile");
 const web3 = new Web3("http://localhost:8545");
 
-const deploy = async (accounts, name, ABI, bytecode) => {
-  try {
-    const contract = await new web3.eth.Contract(JSON.parse(ABI));
-    const deployed = await contract.deploy({ data: bytecode });
-    const sent = await deployed.send({
-     from: accounts[0],
-     gas: 3000000,
-   });
-    const address = sent.options.address;
-    const artifact = { address, abi: JSON.parse(ABI) };
-    await addContractArtifact(name, artifact);
-    console.log("Deployed on:", address);
-    return;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 // This function loops over all compiled contracts and deploys them
 const deployAllContracts = async () => {
   try {
